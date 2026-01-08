9240f609b8c417ee8194752b4c103840185f0ef9
Zero <savyosilv@gmail.com>
2026-01-07 20:40:57 -0500

    ajustando not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link"
 
+
 export default function NotFound() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-10 space-y-4">
